7615588bdf6abfe3674fffdf2f9606e7f248d1e7
chukwuemeka.mbachu <victor.c.mbachu@gmail.com>
2019-12-27 14:34:26 +0100

    done with models

--- app/User.php
+++ app/User.php
@@ -10,13 +10,24 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    const VERIFIED_USER = '1';
+    const UNVERIFIED_USER = '0';
+
+    const ADMIN_USER = 'true';
+    const REGULAR_USER = 'false';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'verified',
+        'verification_token',
+        'admin'
     ];
 
     /**
@@ -25,7 +36,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
+        'verification_token'
     ];
 
     /**
@@ -36,4 +49,22 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function isVerified(){
+        return $this->verified === User::VERIFIED_USER;
+    }
+
+    public function isAdmin(){
+        return $this->admin === User::ADMIN_USER;
+    }
+
+    public static function generateVerificationCode(){
+        $characters = '0123456789';
+        $charactersLength = strlen($characters);
+        $randomString = 'ABCDEFGHIJKL';
+        for ($i = 0; $i < 30; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
 }
